2630e53adb377fdcdbc6619a9b162997b596e1ef
Arrianda <arriandamardhika@gmail.com>
2016-05-08 16:01:27 +0700

    Added files via upload

--- threadizzy.sql
+++ threadizzy.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.14
+-- version 4.2.7.1
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 11 Apr 2016 pada 07.29
--- Versi Server: 5.6.26
--- PHP Version: 5.6.12
+-- Generation Time: 08 Mei 2016 pada 11.00
+-- Versi Server: 5.6.20
+-- PHP Version: 5.5.15
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `threadizzy`
@@ -23,37 +23,299 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `users`
+-- Struktur dari tabel `category`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `username` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `birthday` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL
+CREATE TABLE IF NOT EXISTS `category` (
+`id` int(11) NOT NULL,
+  `nama` varchar(100) NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data untuk tabel `category`
+--
+
+INSERT INTO `category` (`id`, `nama`) VALUES
+(1, 'Hobby');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `comment_status`
+--
+
+CREATE TABLE IF NOT EXISTS `comment_status` (
+`id` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_status` int(11) NOT NULL,
+  `isi` text NOT NULL,
+  `datel` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `comment_thread`
+--
+
+CREATE TABLE IF NOT EXISTS `comment_thread` (
+`id` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_thread` int(11) NOT NULL,
+  `isi` text NOT NULL,
+  `datel` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `post`
+--
+
+CREATE TABLE IF NOT EXISTS `post` (
+`id` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_thread` int(11) NOT NULL,
+  `judul` varchar(100) NOT NULL,
+  `isi` text NOT NULL,
+  `datel` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `rate`
+--
+
+CREATE TABLE IF NOT EXISTS `rate` (
+  `id_user` int(11) NOT NULL,
+  `id_thread` int(11) NOT NULL,
+  `jumlah_bintang` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `status`
+--
+
+CREATE TABLE IF NOT EXISTS `status` (
+`id` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `isi` text NOT NULL,
+  `datel` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `thread`
+--
+
+CREATE TABLE IF NOT EXISTS `thread` (
+`id` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_category` int(11) NOT NULL,
+  `judul` varchar(100) NOT NULL,
+  `datel` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data untuk tabel `thread`
+--
+
+INSERT INTO `thread` (`id`, `id_user`, `id_category`, `judul`, `datel`, `status`) VALUES
+(1, 1, 1, 'testing', '2016-05-08 08:24:04', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+`id` int(11) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `foto` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `gender` varchar(100) NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data untuk tabel `user`
+--
+
+INSERT INTO `user` (`id`, `nama`, `foto`, `email`, `password`, `gender`) VALUES
+(1, 'Arrianda', 'test', 'test', 'test', 'test');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user_followers`
+--
+
+CREATE TABLE IF NOT EXISTS `user_followers` (
+`id` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_followers` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `users`
+-- Indexes for table `category`
 --
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `category`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `comment_status`
+--
+ALTER TABLE `comment_status`
+ ADD PRIMARY KEY (`id`), ADD KEY `id_user` (`id_user`), ADD KEY `id_status` (`id_status`);
+
+--
+-- Indexes for table `comment_thread`
+--
+ALTER TABLE `comment_thread`
+ ADD PRIMARY KEY (`id`), ADD KEY `id_user` (`id_user`), ADD KEY `id_thread` (`id_thread`);
+
+--
+-- Indexes for table `post`
+--
+ALTER TABLE `post`
+ ADD PRIMARY KEY (`id`), ADD KEY `id_user` (`id_user`), ADD KEY `id_thread` (`id_thread`);
+
+--
+-- Indexes for table `rate`
+--
+ALTER TABLE `rate`
+ ADD PRIMARY KEY (`id_user`,`id_thread`), ADD KEY `id_thread` (`id_thread`);
+
+--
+-- Indexes for table `status`
+--
+ALTER TABLE `status`
+ ADD PRIMARY KEY (`id`), ADD KEY `id_user` (`id_user`);
+
+--
+-- Indexes for table `thread`
+--
+ALTER TABLE `thread`
+ ADD PRIMARY KEY (`id`), ADD KEY `id_user` (`id_user`), ADD KEY `id_category` (`id_category`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_followers`
+--
+ALTER TABLE `user_followers`
+ ADD PRIMARY KEY (`id`), ADD KEY `id_user` (`id_user`), ADD KEY `id_followers` (`id_followers`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT for table `category`
 --
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `category`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `comment_status`
+--
+ALTER TABLE `comment_status`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `comment_thread`
+--
+ALTER TABLE `comment_thread`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `post`
+--
+ALTER TABLE `post`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `status`
+--
+ALTER TABLE `status`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `thread`
+--
+ALTER TABLE `thread`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `user_followers`
+--
+ALTER TABLE `user_followers`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `comment_status`
+--
+ALTER TABLE `comment_status`
+ADD CONSTRAINT `comment_status_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`),
+ADD CONSTRAINT `comment_status_ibfk_2` FOREIGN KEY (`id_status`) REFERENCES `status` (`id`);
+
+--
+-- Ketidakleluasaan untuk tabel `comment_thread`
+--
+ALTER TABLE `comment_thread`
+ADD CONSTRAINT `comment_thread_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`),
+ADD CONSTRAINT `comment_thread_ibfk_2` FOREIGN KEY (`id_thread`) REFERENCES `thread` (`id`);
+
+--
+-- Ketidakleluasaan untuk tabel `post`
+--
+ALTER TABLE `post`
+ADD CONSTRAINT `post_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`),
+ADD CONSTRAINT `post_ibfk_2` FOREIGN KEY (`id_thread`) REFERENCES `thread` (`id`);
+
+--
+-- Ketidakleluasaan untuk tabel `rate`
+--
+ALTER TABLE `rate`
+ADD CONSTRAINT `rate_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`),
+ADD CONSTRAINT `rate_ibfk_2` FOREIGN KEY (`id_thread`) REFERENCES `thread` (`id`);
+
+--
+-- Ketidakleluasaan untuk tabel `status`
+--
+ALTER TABLE `status`
+ADD CONSTRAINT `status_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`);
+
+--
+-- Ketidakleluasaan untuk tabel `thread`
+--
+ALTER TABLE `thread`
+ADD CONSTRAINT `thread_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`),
+ADD CONSTRAINT `thread_ibfk_2` FOREIGN KEY (`id_category`) REFERENCES `category` (`id`);
+
+--
+-- Ketidakleluasaan untuk tabel `user_followers`
+--
+ALTER TABLE `user_followers`
+ADD CONSTRAINT `user_followers_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`),
+ADD CONSTRAINT `user_followers_ibfk_2` FOREIGN KEY (`id_followers`) REFERENCES `user` (`id`);
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
